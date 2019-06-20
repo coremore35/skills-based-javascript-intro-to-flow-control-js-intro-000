9b5c095d5c2ad899f8b4be5af47ad4ad2ed0404d
Corey Morrison <corey.neil.morrison@gmail.com>
2019-06-20 03:26:14 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -7,8 +7,12 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
-}
+  if (age < 13) {
+    return ("You are a teenager")
+  } else if (true) {
+    
+  }
+};
 
 function ternaryTeenager(age) {
 
